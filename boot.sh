7718d744d472aa7e443c2cc3d312816d3eaee549
mmcilree <piggy-wiggy11G>
2021-06-04 15:39:28 +0100

    Changed deployment script.

--- boot.sh
+++ boot.sh
@@ -3,9 +3,9 @@
 cp ./nginx.conf /host/mjm42/nginx.d/default/demystify.conf
 nginx -c /host/mjm42/nginx.conf -s reload
 
-echo "Starting tmux session for frontend"
+echo "Starting tmux session for Demystify Visualiser"
 
 kill $(lsof -t -i:3000)
 kill $(lsof -t -i:21586)
-tmux kill-session -t Demystify-frontend
-tmux new-session -d -s Demystify-frontend "npm install && npm run build && node_modules/serve/bin/serve.js -s build -l 21586"
+tmux kill-session -t Demystify-Viz
+tmux new-session -d -s Demystify-Viz "cd client && npm install && npm run build && cd ../server && npm run compile"
