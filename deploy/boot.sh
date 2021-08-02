ed508b2bcc1817bcb8fd2d78aabe19b52ca4b235
mmcilree <mjm42@st-andrews.ac.uk>
2021-08-02 10:11:28 +0100

    Made boot.sh more general.

--- deploy/boot.sh
+++ deploy/boot.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# Change these if necessary
+FLASK_PORT=21586
+REDIS_PORT=6379
+
+# Ensure conjure works correctly
 export PATH=$HOME/.local/bin:$PATH
-cp ./nginx.conf /host/mjm42/nginx.d/default/demystify.conf
-nginx -c /host/mjm42/nginx.conf -s reload
+
+# Export nginx configuration
+cp ./nginx.conf /host/$USER/nginx.d/default/demystify.conf
+nginx -c /host/$USER/nginx.conf -s reload
 
 echo "Setting up python virtual environment."
 cd ../server
@@ -12,11 +19,11 @@ pip3 install -r requirements.txt
 
 cd ..
 
-echo "Killing process on port 21586"
-kill $(lsof -t -i:21586)
+echo "Killing process on port $FLASK_PORT"
+kill $(lsof -t -i:$FLASK_PORT)
 
-echo "Killing process on port 6379"
-kill $(lsof -t -i:6379)
+echo "Killing process on port $REDIS_PORT"
+kill $(lsof -t -i:$REDIS_PORT)
 
 sleep 1
 
@@ -25,10 +32,10 @@ tmux kill-session -t RQWorker
 tmux kill-session -t Demystify-Viz
 
 echo "Starting tmux session for Redis" 
-tmux new-session -d -s Redis "redis-server"
+tmux new-session -d -s Redis "redis-server --port $REDIS_PORT"
 
 echo "Starting tmux session for RQ" 
 tmux new-session -d -s RQWorker "cd server && rq worker"
 
 echo "Starting tmux session for Demystify Visualiser (React/Flask)" 
-tmux new-session -d -s Demystify-Viz "cd client && npm install && npm run build && cd ../server && gunicorn -b 127.0.0.1:21586 api:app --log-level debug"
+tmux new-session -d -s Demystify-Viz "cd client && npm install && npm run build && cd ../server && gunicorn -b 127.0.0.1:$FLASK_PORT api:app --log-level debug"
